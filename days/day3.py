764f56da1254f9ff9377505edb1449c0d9da7817
Lloyd Lobo <lloydlobo4@gmail.com>
2023-12-07 20:19:40 +0530

    add day3 part 2 solution

--- days/day3.py
+++ days/day3.py
@@ -7,7 +7,9 @@
 #
 # --- Part 1 ---
 #
-# To find the sum of all part numbers in the engine schematic, you need to consider each number adjacent to a symbol, including diagonally. Periods (.) do not count as symbols.
+# To find the sum of all part numbers in the engine schematic,
+# you need to consider each number adjacent to a symbol,
+# including diagonally. Periods (.) do not count as symbols.
 #
 # Let's calculate the sum based on the provided example:
 #
@@ -31,37 +33,37 @@
 # their sum is 4361.
 #
 # --- Part 2 ---
+
+# You've got a machine with some gears that affect its speed.
+# There are symbols (*) next to two numbers,
+# and multiplying those numbers together gives you a gear ratio.
+# Your task is to find the gear ratios for each gear and add them up.
 #
-
-
+# Look at the numbers around the star (*) symbols, multiply the two
+# adjacent numbers, and then add all those gear ratios together.
+# The total of these gear ratios will help identify which gear needs fixing.
+#
 # https://www.reddit.com/r/adventofcode/comments/189m3qw/2023_day_3_solutions/
 #
 # Wow! This makes two assumptions that ended up being true for the input:
 # - No number touches more than one symbol
 # - No non-star symbol touches exactly two numbers
 
-import math as m
+import math
 import os
-from re import finditer
+from pprint import pprint
 from typing import List, TextIO
 
 FILEPATH = os.path.join('input', 'day3')
 
 
 def check() -> int:
-    # assert (part1_old(data=(
-    #     "467..114..\n" "...*......\n" "..35..633.\n" "......#...\n" "617*......\n" ".....+.58.\n" "..592.....\n" "......755.\n" "...$.*....\n" ".664.598..\n")
-    # ) == 4361)  # Works
     infile: TextIO
     with open(FILEPATH) as infile:
         assert (part1(infile) == 535235)
+        assert (part2(infile) == 79844424)
 
     return 0
-
-
-# def is_symbol(char):
-#     assert len(char) == 1
-#     return char.isascii() and char != '.' and not char.isdigit()
 
 
 def part1(infile: TextIO) -> int:
@@ -74,6 +76,7 @@ def part1(infile: TextIO) -> int:
 
     for r, row in enumerate(grid):
         buf = ''
+
         for c, col_val in enumerate(row):
             if col_val.isdigit():
                 buf += col_val
@@ -85,15 +88,40 @@ def part1(infile: TextIO) -> int:
                     chars[o].append(int(buf))
 
                 buf = ''
-
+    # pprint(chars)
     sum_part_nums = sum(sum(p) for p in chars.values())
     # print(sum_part_nums)
 
     return sum_part_nums
 
 
-# def part2(data: str) -> int:
-# return sum(m.prod(p) for p in chars.values() if len(p) == 2)
+def part2(infile: TextIO) -> int:
+    grid = list(open(FILEPATH))
+
+    max_i, max_j = len(grid), len(grid[0].strip())
+
+    chars: dict = {(r, c): [] for r in range(max_i) for c in range(max_j) if
+                   grid[r][c] not in '0123456789.'}
+
+    for r, row in enumerate(grid):
+        buf = ''
+
+        for c, col_val in enumerate(row):
+            if col_val.isdigit():
+                buf += col_val
+            elif buf:
+                edge = {(r, c_idx) for r in (r - 1, r, r + 1) for c_idx in
+                        range(max(0, c - len(buf) - 1), min(max_j, c + 1))}
+
+                for o in edge & chars.keys():
+                    chars[o].append(int(buf))
+
+                buf = ''
+    # pprint(chars)
+
+    product_part_nums = sum(math.prod(p) for p in chars.values() if len(p) == 2)
+    # print(product_part_nums)
+    return product_part_nums
 
 
 def part1_old(data: str) -> int:
@@ -133,14 +161,38 @@ def part1_old(data: str) -> int:
 
                 char_buf.clear()
 
-    # print('all_nums:', all_nums)
-    # print('nums:', nums)
-    # print('sum of nums:', sum(nums))
     return sum(nums)
 
 
 """
 chars = {(0, 3): [], (0, 4): [], (0, 8): [], (0, 9): [], (1, 0): [], (1, 1): [], (1, 2): [], (1, 3): [], (1, 4): [], (1, 5): [], (1, 6): [], (1, 7): [], (1, 8): [], (1, 9): [], (2, 0): [], (2, 1): [], (2, 4): [], (2, 5): [], (2, 9): [], (3, 0): [], (3, 1): [], (3, 2): [], (3, 3): [], (3, 4): [], (3, 5): [], (3, 6): [], (3, 7): [], (3, 8): [], (3, 9): [], (4, 3): [], (4, 4): [], (4, 5): [], (4, 6): [], (4, 7): [], (4, 8): [], (4, 9): [], (5, 0): [], (5, 1): [], (5, 2): [], (5, 3): [], (5, 4): [], (5, 5): [], (5, 6): [], (5, 9): [], (6, 0): [], (6, 1): [], (6, 5): [], (6, 6): [], (6, 7): [], (6, 8): [], (6, 9): [], (7, 0): [], (7, 1): [], (7, 2): [], (7, 3): [], (7, 4): [], (7, 5): [], (7, 9): [], (8, 0): [], (8, 1): [], (8, 2): [], (8, 3): [], (8, 4): [], (8, 5): [], (8, 6): [], (8, 7): [], (8, 8): [], (8, 9): [], (9, 0): [], (9, 4): [], (9, 8): [], (9, 9): []}
+{(1, 7): [954],
+ (1, 27): [52, 398],
+ (1, 57): [806, 983],
+ (1, 95): [217],
+ (2, 62): [664, 943],
+ (2, 79): [677, 6],
+ (2, 102): [459, 687],
+ (2, 114): [687],
+ (3, 37): [548],
+ (3, 45): [495, 488],
+ (3, 85): [282, 773],
+ (3, 106): [973],
+ (3, 112): [2, 244],
+ (3, 118): [248],
+ (3, 123): [409, 311],
+ (3, 128): [722, 286],
+ (3, 133): [165],
+ (4, 5): [261],
+ (4, 10): [663],
+ (4, 33): [704, 842],
+ (4, 40): [268, 696],
+ (5, 20): [442],
+ (5, 28): [828, 563],
+ (5, 54): [534],
+ (5, 65): [512],
+ ...
+
 """
 
 """
