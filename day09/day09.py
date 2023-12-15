e65e550cf09c50b4544a2f3f48c1c48829769662
Lloyd Lobo <lloydlobo4@gmail.com>
2023-12-16 05:22:02 +0530

    add day 9 part 2 solution

--- day09/day09.py
+++ day09/day09.py
@@ -1,6 +1,7 @@
 # day9.py
 #
 # --- Day 9: Mirage Maintenance ---
+from typing import List
 
 from utils import *
 
@@ -8,29 +9,31 @@ from utils import *
 0 3 6 9 12 15
 1 3 6 10 15 21
 10 13 16 21 30 45
-
 """
 
 
 def part1(data):
     D = data.strip()
-    ldata = parse_data(D)
-    diffs = [consec_diffs(lst=lst) for lst in ldata]
+    ldata: list[list[int]] = parse_data(D)
+    diffs: list[list[list[int]]] = [consec_diffs(lst=lst) for lst in ldata]
     p1 = solve(ldata, diffs)
     print(p1)  # 114 | 1953784198
 
 
 def part2(data):
-    if False:
-        D = data.strip()
-        ldata = parse_data(D)
-        diffs = [consec_diffs(lst=lst) for lst in ldata]
-        p2 = solve(ldata, diffs)
-        print(p2)  # ? | ?
+    D = data.strip()
+    d: list[list[int]] = parse_data(D)
+    ldata = []
+    for x in d:
+        x.reverse()
+        ldata.append(x)
+    diffs: list[list[list[int]]] = [consec_diffs(lst=lst) for lst in ldata]
+    p2 = solve(ldata, diffs)
+    print(p2)  # 2 | 957
 
 
-def consec_diffs(lst: list[int]):
-    diffs: list[list[int]] = []
+def consec_diffs(lst: list[int]) -> list[list[int]]:
+    diffs = []
     while len(lst) > 1:
         diff = [lst[i + 1] - lst[i] for i in range(len(lst) - 1)]
         diffs.append(diff)
@@ -38,71 +41,49 @@ def consec_diffs(lst: list[int]):
     return diffs
 
 
-def solve(ldata, diffs):
+def solve(ldata: list[list[int]], diffs: list[list[list[int]]]):
     """
     Analyze your OASIS report and extrapolate the next value for each history.
     What is the sum of these extrapolated values?
     """
-
-    before_data = join_head_diffs(diffs, ldata)
-    # pprint(('before_data:', before_data))
     stats = {}
+
+    # diff: list[list[int]]
+    # idx: int
     for idx, diff in enumerate(diffs):
-        buf = []
-        glvl = 0
-        gdiff = 0
+        stat_lst = []
+        glvl, gdiff = 0, 0
+
         for i, d in enumerate(diff):
-            unq_diff = len(set(d))
-            lvl = i + 1
-            print(d)
-            diffstasis = d[0] - d[1]
-            gdiff = diffstasis
-            glvl = lvl
-
-            str_all_0 = [1 for iint in d if iint == 0]
-            has_all_0s = (len(str_all_0) > 0 and all(str_all_0) == 1)
-            is_valid = unq_diff == 1
-            statval = dict(lvl=lvl, homeostasis=diffstasis if not is_valid else d[0], jstr=ljoinstr(d))
-
+            diff_stasis, lvl = (d[0] - d[1]), (i + 1)
+            gdiff, glvl = diff_stasis, lvl
+            # str_all_0 = [1 for iint in d if iint == 0]
+            # has_all_0s = (len(str_all_0) > 0 and all(str_all_0) == 1)
+            is_valid: bool = len(set(d)) == 1
+            statval = dict(lvl=lvl, jstr=ljoinstr(d), diff_stasis=(diff_stasis if not is_valid else d[0]))
             if is_valid:
-                buf.append(statval)
-                # dict(stable_val=d_str[0], d_str=d_str, i=i)
+                stat_lst.append(statval)
                 break
-            # if has_all_0s:
-            #     break
 
-        statval = dict(lvl=glvl, homeostasis=gdiff, jstr=ljoinstr(diff))
-        stats[idx] = buf[0] if buf else statval
-        # stats[idx] = buf[0]
+        statval = dict(lvl=glvl, diff_stasis=gdiff, jstr=ljoinstr(diff))
+        stats[idx] = stat_lst[0] if stat_lst else statval
+    # pprint(stats)
 
-    # stats = {
-    #     idx: [dict(lvl=(i + 1), homeostasis=ljoinstr(d)[0], jstr=ljoinstr(d))
-    #           for i, d in enumerate(diff)
-    #           if len(set(ljoinstr(d))) == 1][0]
-    #     for idx, diff in enumerate(diffs)
-    # }
-    pprint(stats)
-    # pprint((ldata, diffs))
-
-    extrapolated_data = []
+    D_JOIN = join_head_diffs(diffs, ldata)
     derived_ancestors = []
-    for ikey, (key, val) in enumerate(before_data.items()):
+
+    for ikey, (key, val) in enumerate(D_JOIN.items()):
         assert ikey == key and 'Should have original iterable index/key'
         revval = copy.deepcopy(val)[::-1]
         cur_stat = stats[key]
-        pprint(cur_stat)
-
-        stable_val, stable_lvl_from_head = int(cur_stat['homeostasis']), cur_stat['lvl']
-        if revval[0][0] != stable_val:
-            print((revval[0], stable_val))
+        stable_val, stable_lvl_from_head = int(cur_stat['diff_stasis']), cur_stat['lvl']
         assert revval[0][0] == stable_val and 'All ints at this level must be same'
         assert len(val) == (stable_lvl_from_head + 1) and 'After stable level ommited all 0s'
-
         prev = stable_val
 
         for i, v in enumerate(revval):
             cur = v
-            # print(key, i, prev, v)
+
             if i == 0:
                 assert cur[0] == prev == cur[-1]
                 assert revval[i][0] == prev
@@ -110,19 +91,26 @@ def solve(ldata, diffs):
                 revval[i].append(prev)
             elif i < len(revval):
                 assert v[-1] == revval[i][-1]
-                exsum = v[-1] + prev
-                revval[i].append(exsum)
-                prev = revval[i][-1]
-        extrapolated_data.append(revval)
+                nxt = v[-1] + prev
+                revval[i].append(nxt)
+                prev = nxt
+
         derived_ancestors.append(prev)
 
-    # pprint(('extrapolated_data:', extrapolated_data))
     # pprint(('derived_ancestors:', derived_ancestors))
-
     return sum(derived_ancestors)
 
 
 def join_head_diffs(diffs, ldata):
+    """
+    def join_head_diffs(diffs, ldata):
+        ND = {}
+        for i, ds in enumerate(diffs):
+            nnlst = [ldata[i]]
+            nnlst.extend(d for d in ds if not any(d) == 0)
+            ND[i] = nnlst
+        return ND
+    """
     ND = {}
     for i, ds in enumerate(diffs):
         nnlst = [ldata[i]]
@@ -133,7 +121,11 @@ def join_head_diffs(diffs, ldata):
     return ND
 
 
-def parse_data(data: str) -> Any:
+def parse_data(data: str) -> list[list[int]]:
+    """
+    def parse_data(data: str) -> list[list[int]]:
+        return [list(map(int, line.split())) for line in data.splitlines()]
+    """
     lines: map[list[int]] = map(ints, data.splitlines())
     return list(lines)
 
@@ -153,10 +145,10 @@ profile_stats = 'profile_stats_check'
 cProfile.run('check(part1, part2)', profile_stats)
 stats = pstats.Stats(profile_stats)
 stats.sort_stats('cumulative')
-# stats.print_stats(3)
+stats.print_stats(10)
 """
-Fri Dec 15 13:24:19 2023    profile_stats_check
-         20 function calls in 0.003 seconds
+Fri Dec 15 19:04:03 2023    profile_stats_check
+         249645 function calls (213406 primitive calls) in 0.192 seconds
 """
 
 """
