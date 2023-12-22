41b0392f42071a11e6f8b46c44b63dcbda24252d
Lloyd Lobo <lloydlobo4@gmail.com>
2023-12-22 21:31:15 +0530

    add day 15 part 2 solution

--- day15/day15.py
+++ day15/day15.py
@@ -1,24 +1,6 @@
+# day15.py
 from utils import *
 
-"""
-rn=1 becomes 30.
-cm- becomes 253.
-qp=3 becomes 97.
-cm=2 becomes 47.
-qp- becomes 14.
-pc=4 becomes 180.
-ot=9 becomes 9.
-ab=5 becomes 197.
-pc- becomes 48.
-pc=6 becomes 214.
-ot=7 becomes 231.
-In this example, the sum of these results is 1320. Unfortunately, the reindeer has stolen the page containing the expected verification number and is currently running around the facility with it excitedly.
-
-Run the HASH algorithm on each step in the initialization sequence. What is the sum of the results? (The initialization sequence is one long line; be careful when copy-pasting it.)
-"""
-
-
-# ----------------------------------------------------------------------------------------------------------------------
 
 def hash_simple(s: str) -> int:
     """ assert hash_simple('HASH') == 52 """
@@ -31,14 +13,35 @@ def hash_simple(s: str) -> int:
 
 
 def solve(sequences, for_part2=False):
-    if for_part2:
-        return -1
-
-    sum_sequences = 0
-    for i, sequence in enumerate(sequences):
-        sum_sequences += hash_simple(sequence)
-
-    return sum_sequences
+    if not for_part2:
+        return sum([hash_simple(sequence) for i, sequence in enumerate(sequences)])
+    else:
+        library: dict[int, list[str]] = {i: [] for i in range(256)}
+        for sequence in sequences:
+            if '=' in sequence:
+                label = sequence[:sequence.index('=')]
+                box_id = hash_simple(label)
+                found_duplicate = False
+                for lens_index, lens in enumerate(library[box_id]):
+                    if lens.startswith(label):
+                        library[box_id][lens_index] = sequence
+                        found_duplicate = True
+                if not found_duplicate:
+                    library[box_id].append(sequence)
+            elif '-' in sequence:
+                label = sequence[:sequence.index('-')]
+                box_id = hash_simple(label)
+                for lens in library[box_id]:
+                    if lens.startswith(label):
+                        library[box_id].remove(lens)
+        total = 0
+        valid_libs = list((i, x) for i, x in enumerate(library.values()) if x)
+        for box_id, lenses in valid_libs:
+            for lens_idx, lens in enumerate(lenses):
+                idx_eq = lens.index('=')
+                focal_len = int(lens[(idx_eq + 1):])
+                total += (box_id + 1) * (lenses.index(lens) + 1) * (focal_len)
+        return total
 
 
 # ----------------------------------------------------------------------------------------------------------------------
@@ -46,16 +49,14 @@ def solve(sequences, for_part2=False):
 
 def part1(data):
     input_data = [data.split(',')][0]
-    print(f'{input_data=}')
-
     p1 = solve(input_data, for_part2=False)
     print(f'{p1=}')  # 1320 | 510273
 
 
 def part2(data):
-    input_data = strtogrid(copy.deepcopy(data))
+    input_data = [data.split(',')][0]
     p2 = solve(input_data, for_part2=True)
-    print(f'{p2=}')  # ? | ?
+    print(f'{p2=}')  # 145 | 212449
 
 
 # ----------------------------------------------------------------------------------------------------------------------
